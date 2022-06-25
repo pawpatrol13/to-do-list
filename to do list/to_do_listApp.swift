ea431be8416e4a293240c7cb79bf7a2c78b1ca82
Angus chua <angusc8774@gmail.com>
2022-06-25 10:15:28 +0800

    Initial Commit

---
+++ to do list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to do list
+//
+//  Created by T Krobot on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
